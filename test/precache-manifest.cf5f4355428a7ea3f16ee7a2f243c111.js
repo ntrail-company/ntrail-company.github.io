f3f5954844ff19e46018d978fe42b91d42a54c72
root <borisoffficial@gmail.com>
2019-12-25 21:31:55 +0300

    auto commit

---
+++ test/precache-manifest.cf5f4355428a7ea3f16ee7a2f243c111.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440d642f6f48bf7fa945b58b96a9b96f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1b2f985fdb8088e06b97",
+    "url": "./static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "16fcc29263be06165a35",
+    "url": "./static/css/main.cb0bc549.chunk.css"
+  },
+  {
+    "revision": "1b2f985fdb8088e06b97",
+    "url": "./static/js/2.ec2c4e44.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.ec2c4e44.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16fcc29263be06165a35",
+    "url": "./static/js/main.8e767d26.chunk.js"
+  },
+  {
+    "revision": "fb548bef2165e415602b",
+    "url": "./static/js/runtime-main.55213b03.js"
+  }
+]);
