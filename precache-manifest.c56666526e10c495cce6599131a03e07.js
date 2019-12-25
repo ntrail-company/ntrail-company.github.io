5979e9e4ab81cf23e56bcd049972f188ccc151a6
boris <borisoffficial@gmail.com>
2019-12-25 20:51:11 +0300

    auto commit

---
+++ precache-manifest.c56666526e10c495cce6599131a03e07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1854fae18bc38e21b64d15eea28f0b3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1b2f985fdb8088e06b97",
+    "url": "./static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "8c1969c940b878fef6af",
+    "url": "./static/css/main.93ca8f54.chunk.css"
+  },
+  {
+    "revision": "1b2f985fdb8088e06b97",
+    "url": "./static/js/2.ec2c4e44.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.ec2c4e44.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c1969c940b878fef6af",
+    "url": "./static/js/main.8e767d26.chunk.js"
+  },
+  {
+    "revision": "fb548bef2165e415602b",
+    "url": "./static/js/runtime-main.55213b03.js"
+  }
+]);
